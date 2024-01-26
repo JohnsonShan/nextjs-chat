e66aed845e3d955d06c27dd3b929502ed29abbf9
JohnsonMac <b081148@gmail.com>
2024-01-26 16:37:02 -0500

    add google login, fix seesion id

--- auth.ts
+++ auth.ts
@@ -33,20 +33,26 @@ export const {
     })],
   callbacks: {
     jwt({ token, profile }) {
+      // console.log('jwt: token, profile', token, profile)
       if (profile) {
-        // console.log(profile)
         token.id = profile.id
         token.image = profile.avatar_url || profile.picture
       }
       return token
     },
     session: ({ session, token }) => {
-      if (session?.user && token?.id) {
-        session.user.id = String(token.id)
+      // console.log('session: session, token', session, token)
+      if (session?.user) {
+        if (token?.id) {
+          session.user.id = String(token.id)
+        } else if (token?.sub) {
+          session.user.id = String(token.sub)
+        }
       }
       return session
     },
     authorized({ auth }) {
+      // console.log('auth: auth', auth)
       return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
